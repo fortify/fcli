407e4af8db3479a9610cee24e7e43989d91ebe41
Ruud Senden <ruud.senden@microfocus.com>
2021-10-23 12:01:29 +0200

    Initial implementations for get ssc events/apps/versions

--- fcli-ssc/src/main/java/com/fortify/cli/ssc/command/entity/SSCApplicationVersionCommands.java
+++ fcli-ssc/src/main/java/com/fortify/cli/ssc/command/entity/SSCApplicationVersionCommands.java
@@ -24,51 +24,73 @@
  ******************************************************************************/
 package com.fortify.cli.ssc.command.entity;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fortify.cli.command.util.SubcommandOf;
+import com.fortify.cli.ssc.command.AbstractSSCUnirestRunnerCommand;
 import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCCreateCommand;
 import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCDeleteCommand;
 import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCGetCommand;
 import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCUpdateCommand;
 
 import jakarta.inject.Singleton;
+import kong.unirest.UnirestInstance;
+import lombok.SneakyThrows;
 import picocli.CommandLine.Command;
 
-public class SSCApplicationEntityCommands {
-	private static final String ENTITY = "application";
+public class SSCApplicationVersionCommands {
+	private static final String NAME = "application-versions";
+	private static final String DESC = "application versions";
 	
 	@Singleton
 	@SubcommandOf(SSCGetCommand.class)
-	@Command(name = ENTITY, description = "Get "+ENTITY+" data from SSC")
-	public static final class Get implements Runnable {
-		@Override
-		public void run() {
+	@Command(name = NAME, description = "Get "+DESC+" from SSC")
+	public static final class Get extends AbstractSSCUnirestRunnerCommand {
+		@SneakyThrows
+		protected Void runWithUnirest(UnirestInstance unirest) {
+			System.out.println(
+				unirest.get("/api/v1/projectVersions?limit=-1")
+					.accept("application/json")
+					.header("Content-Type", "application/json")
+					.asObject(ObjectNode.class)
+					.getBody()
+					.get("data")
+					.toPrettyString()
+			);
+			
+			return null;
 		}
 	}
 	
 	@Singleton
 	@SubcommandOf(SSCCreateCommand.class)
-	@Command(name = ENTITY, description = "Create "+ENTITY+" in SSC")
-	public static final class Create implements Runnable {
-		@Override
-		public void run() {
+	@Command(name = NAME, description = "Create "+DESC+" in SSC")
+	public static final class Create extends AbstractSSCUnirestRunnerCommand {
+		@SneakyThrows
+		protected Void runWithUnirest(UnirestInstance unirest) {
+			System.err.println("ERROR: Not yet implemented");
+			return null;
 		}
 	}
 	
 	@Singleton
 	@SubcommandOf(SSCUpdateCommand.class)
-	@Command(name = ENTITY, description = "Update "+ENTITY+" in SSC")
-	public static final class Update implements Runnable {
-		@Override
-		public void run() {
+	@Command(name = NAME, description = "Update "+DESC+" in SSC")
+	public static final class Update extends AbstractSSCUnirestRunnerCommand {
+		@SneakyThrows
+		protected Void runWithUnirest(UnirestInstance unirest) {
+			System.err.println("ERROR: Not yet implemented");
+			return null;
 		}
 	}
 	
 	@Singleton
 	@SubcommandOf(SSCDeleteCommand.class)
-	@Command(name = ENTITY, description = "Delete "+ENTITY+" from SSC")
-	public static final class Delete implements Runnable {
-		@Override
-		public void run() {
+	@Command(name = NAME, description = "Delete "+DESC+" from SSC")
+	public static final class Delete extends AbstractSSCUnirestRunnerCommand {
+		@SneakyThrows
+		protected Void runWithUnirest(UnirestInstance unirest) {
+			System.err.println("ERROR: Not yet implemented");
+			return null;
 		}
 	}
 }
--- fcli-ssc/src/main/java/com/fortify/cli/ssc/command/entity/SSCSystemEventCommands.java
+++ fcli-ssc/src/main/java/com/fortify/cli/ssc/command/entity/SSCSystemEventCommands.java
@@ -24,51 +24,37 @@
  ******************************************************************************/
 package com.fortify.cli.ssc.command.entity;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fortify.cli.command.util.SubcommandOf;
-import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCCreateCommand;
-import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCDeleteCommand;
+import com.fortify.cli.ssc.command.AbstractSSCUnirestRunnerCommand;
 import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCGetCommand;
-import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands.SSCUpdateCommand;
 
 import jakarta.inject.Singleton;
+import kong.unirest.UnirestInstance;
+import lombok.SneakyThrows;
 import picocli.CommandLine.Command;
 
-public class SSCApplicationVersionEntityCommands {
-	public static final String ENTITY = "version";
+public class SSCSystemEventCommands {
+    private static final String NAME = "system-events";
+    private static final String DESC = "system events";
 	
 	@Singleton
 	@SubcommandOf(SSCGetCommand.class)
-	@Command(name = ENTITY, description = "Get "+ENTITY+" data from SSC")
-	public static final class Get implements Runnable {
-		@Override
-		public void run() {
-		}
-	}
-	
-	@Singleton
-	@SubcommandOf(SSCCreateCommand.class)
-	@Command(name = ENTITY, description = "Create "+ENTITY+" in SSC")
-	public static final class Create implements Runnable {
-		@Override
-		public void run() {
-		}
-	}
-	
-	@Singleton
-	@SubcommandOf(SSCUpdateCommand.class)
-	@Command(name = ENTITY, description = "Update "+ENTITY+" in SSC")
-	public static final class Update implements Runnable {
-		@Override
-		public void run() {
-		}
-	}
-	
-	@Singleton
-	@SubcommandOf(SSCDeleteCommand.class)
-	@Command(name = ENTITY, description = "Delete "+ENTITY+" from SSC")
-	public static final class Delete implements Runnable {
-		@Override
-		public void run() {
+	@Command(name = NAME, description = "Get "+DESC+" data from SSC")
+	public static final class Get extends AbstractSSCUnirestRunnerCommand {
+		@SneakyThrows
+		protected Void runWithUnirest(UnirestInstance unirest) {
+			System.out.println(
+				unirest.get("/api/v1/events?limit=-1")
+					.accept("application/json")
+					.header("Content-Type", "application/json")
+					.asObject(ObjectNode.class)
+					.getBody()
+					.get("data")
+					.toPrettyString()
+			);
+			
+			return null;
 		}
 	}
 }
