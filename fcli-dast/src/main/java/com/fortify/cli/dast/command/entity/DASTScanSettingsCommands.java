a427d6088c3b9356d921679251ddfdf959cd7552
Alex Gendry <alex.gendry@microfocus.com>
2021-10-25 15:08:33 +0000

    docs: DASTScanSettingsCommands created

---
+++ fcli-dast/src/main/java/com/fortify/cli/dast/command/entity/DASTScanSettingsCommands.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * (c) Copyright 2020 Micro Focus or one of its affiliates
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+package com.fortify.cli.dast.command.entity;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fortify.cli.common.command.util.annotation.RequiresProduct;
+import com.fortify.cli.common.command.util.annotation.SubcommandOf;
+import com.fortify.cli.common.config.product.Product;
+import com.fortify.cli.ssc.command.AbstractSSCUnirestRunnerCommand;
+import com.fortify.cli.ssc.command.entity.SSCEntityRootCommands;
+import jakarta.inject.Singleton;
+import kong.unirest.UnirestInstance;
+import lombok.SneakyThrows;
+import picocli.CommandLine;
+
+public class DASTScanSettingsCommands {
+    private static final String NAME = "scan-settings";
+    private static final String DESC = "DAST scan settings list";
+
+    @Singleton
+    @SubcommandOf(SSCEntityRootCommands.SSCGetCommand.class)
+    @CommandLine.Command(name = NAME, description = "Get "+DESC+" from SSC")
+    @RequiresProduct(Product.SSC)
+    @RequiresProduct(Product.SC_DAST)
+    public static final class Get extends AbstractSSCUnirestRunnerCommand {
+        @SneakyThrows
+        protected Void runWithUnirest(UnirestInstance unirest) {
+
+            return null;
+        }
+    }
+}
