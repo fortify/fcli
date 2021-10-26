3fbde8e77c25339135a8b9d94c6b845ad68bb11c
Alex Gendry <alex.gendry@microfocus.com>
2021-10-26 21:45:37 +0000

    wip: filter ouput using xpath or jsonpath

--- fcli-common/src/main/java/com/fortify/cli/common/output/filter/xPathOutputFilter.java
+++ fcli-common/src/main/java/com/fortify/cli/common/output/filter/xPathOutputFilter.java
@@ -1,14 +1,47 @@
+<<<<<<< HEAD
+=======
+/*******************************************************************************
+ * (c) Copyright 2020 Micro Focus or one of its affiliates
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the 
+ * "Software"), to deal in the Software without restriction, including without 
+ * limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to 
+ * whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY 
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+>>>>>>> a07c2bb (wip: filter ouput using xpath or jsonpath)
 package com.fortify.cli.common.output.filter;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+<<<<<<< HEAD
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fortify.cli.common.output.writer.IOutputWriter;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+=======
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fortify.cli.common.output.writer.IOutputWriter;
+>>>>>>> a07c2bb (wip: filter ouput using xpath or jsonpath)
 import lombok.SneakyThrows;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -17,7 +50,10 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 import java.io.StringReader;
+<<<<<<< HEAD
 import java.util.List;
+=======
+>>>>>>> a07c2bb (wip: filter ouput using xpath or jsonpath)
 
 public class xPathOutputFilter implements IOutputFilter {
 
