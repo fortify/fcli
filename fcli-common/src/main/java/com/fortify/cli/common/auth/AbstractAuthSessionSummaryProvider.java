73094c5124962a11f161fd5d6851d2a01fddeeff
Ruud Senden <ruud.senden@microfocus.com>
2021-10-28 09:15:18 +0200

    Updated/added copyright statements

--- fcli-common/src/main/java/com/fortify/cli/common/auth/AbstractAuthSessionSummaryProvider.java
+++ fcli-common/src/main/java/com/fortify/cli/common/auth/AbstractAuthSessionSummaryProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * (c) Copyright 2020 Micro Focus or one of its affiliates
+ * (c) Copyright 2021 Micro Focus or one of its affiliates
  *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the 
