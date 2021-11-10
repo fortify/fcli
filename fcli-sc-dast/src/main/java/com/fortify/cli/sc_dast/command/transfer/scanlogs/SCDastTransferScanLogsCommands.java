7f8a3bbcc01d1cfd390e01b9b454f8e63836dbe7
Alex Gendry <alex.gendry@microfocus.com>
2021-11-10 20:31:59 +0000

    feature added : download scan-logs (logs.zip)

--- fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/command/transfer/scanlogs/SCDastTransferScanLogsCommands.java
+++ fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/command/transfer/scanlogs/SCDastTransferScanLogsCommands.java
@@ -1,4 +1,4 @@
-package com.fortify.cli.sc_dast.command.transfer.scanresults;
+package com.fortify.cli.sc_dast.command.transfer.scanlogs;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fortify.cli.common.picocli.annotation.SubcommandOf;
@@ -8,8 +8,8 @@ import com.fortify.cli.common.picocli.component.output.OutputOptionsWriterConfig
 import com.fortify.cli.sc_dast.command.AbstractSCDastUnirestRunnerCommand;
 import com.fortify.cli.sc_dast.command.crud.SCDastCrudRootCommands.SCDastGetCommand;
 import com.fortify.cli.sc_dast.command.transfer.SCDastTransferRootCommands;
-import com.fortify.cli.sc_dast.command.transfer.scanresults.actions.SCDastTransferScanResultsActionsHandler;
-import com.fortify.cli.sc_dast.command.transfer.scanresults.options.SCDastTransferScanResultsOptions;
+import com.fortify.cli.sc_dast.command.transfer.scanlogs.actions.SCDastTransferScanLogsActionsHandler;
+import com.fortify.cli.sc_dast.command.transfer.scanlogs.options.SCDastTransferScanLogsOptions;
 import io.micronaut.core.annotation.ReflectiveAccess;
 import kong.unirest.UnirestInstance;
 import lombok.Getter;
@@ -18,9 +18,9 @@ import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
 
-public class SCDastTransferScanResultsCommands {
-    private static final String NAME = "scan-results";
-    private static final String DESC = "DAST scan results";
+public class SCDastTransferScanLogsCommands {
+    private static final String NAME = "scan-logs";
+    private static final String DESC = "DAST scan logs";
 
     private static final String _getDefaultOutputColumns() {
         return  "path";
@@ -31,17 +31,17 @@ public class SCDastTransferScanResultsCommands {
     @Command(name = NAME, description = "Download " + DESC + " from SC DAST")
     public static final class Download extends AbstractSCDastUnirestRunnerCommand implements IOutputOptionsWriterConfigSupplier {
 
-        @ArgGroup(exclusive = false, heading = "Download results from a specific scan:%n", order = 1)
-        @Getter private SCDastTransferScanResultsOptions scanResultsOptions;
+        @ArgGroup(exclusive = false, heading = "Download logs from a specific scan:%n", order = 1)
+        @Getter private SCDastTransferScanLogsOptions scanLogsOptions;
 
         @Mixin
         @Getter private OutputOptionsHandler outputOptionsHandler;
 
         @SneakyThrows
         protected Void runWithUnirest(UnirestInstance unirest){
-            SCDastTransferScanResultsActionsHandler actionsHandler = new SCDastTransferScanResultsActionsHandler(unirest);
+            SCDastTransferScanLogsActionsHandler actionsHandler = new SCDastTransferScanLogsActionsHandler(unirest);
 
-            ObjectNode result = actionsHandler.downloadScanResults(scanResultsOptions.getScanId(), scanResultsOptions.getFile());
+            ObjectNode result = actionsHandler.downloadScanLogs(scanLogsOptions.getScanId(), scanLogsOptions.getFile());
 
             outputOptionsHandler.write(result);
 
--- fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/command/transfer/scanlogs/actions/SCDastTransferScanLogsActionsHandler.java
+++ fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/command/transfer/scanlogs/actions/SCDastTransferScanLogsActionsHandler.java
@@ -1,31 +1,25 @@
-package com.fortify.cli.sc_dast.command.transfer.scanresults.actions;
+package com.fortify.cli.sc_dast.command.transfer.scanlogs.actions;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fortify.cli.sc_dast.command.crud.scan.actions.SCDastScanActionsHandler;
-import com.fortify.cli.sc_dast.command.crud.scanstatus.actions.SCDastScanStatusActionsHandler;
 import io.micronaut.core.annotation.ReflectiveAccess;
 import jakarta.inject.Inject;
-import kong.unirest.HttpResponse;
 import kong.unirest.UnirestInstance;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.SneakyThrows;
 
 import java.io.File;
 
 @ReflectiveAccess
-public class SCDastTransferScanResultsActionsHandler {
+public class SCDastTransferScanLogsActionsHandler {
     @Getter @Inject
     UnirestInstance unirest;
 
-    public SCDastTransferScanResultsActionsHandler(UnirestInstance unirest) {
+    public SCDastTransferScanLogsActionsHandler(UnirestInstance unirest) {
         this.unirest = unirest;
     }
 
-    public ObjectNode downloadScanResults(int scanId, File file) {
-        String urlPath = "/api/v2/scans/"+ scanId + "/download-results";
+    public ObjectNode downloadScanLogs(int scanId, File file) {
+        String urlPath = "/api/v2/scans/"+ scanId + "/download-logs";
         getUnirest().get(urlPath)
                 .accept("application/json")
                 .header("Content-Type", "application/json")
