fdedceb4d530526d6fa43ab41f2f87b1c6b24d54
Mike Snowden <5297545+wtfacoconut@users.noreply.github.com>
2022-08-21 00:31:48 +1000

    Test with jline.

--- fcli-ssc/src/main/java/com/fortify/cli/ssc/picocli/command/appversion/SSCAppVersionCreateWizardCommand.java
+++ fcli-ssc/src/main/java/com/fortify/cli/ssc/picocli/command/appversion/SSCAppVersionCreateWizardCommand.java
@@ -24,6 +24,7 @@
  ******************************************************************************/
 package com.fortify.cli.ssc.picocli.command.appversion;
 
+import com.ctc.wstx.shaded.msv_core.util.LightStack;
 import com.fortify.cli.common.picocli.mixin.output.IOutputConfigSupplier;
 import com.fortify.cli.common.picocli.mixin.output.OutputConfig;
 import com.fortify.cli.common.picocli.mixin.output.OutputMixin;
@@ -31,11 +32,23 @@ import com.fortify.cli.ssc.picocli.command.AbstractSSCUnirestRunnerCommand;
 import com.fortify.cli.ssc.picocli.mixin.filter.SSCFilterMixin;
 import com.fortify.cli.ssc.util.SSCOutputHelper;
 import io.micronaut.core.annotation.ReflectiveAccess;
-import jexer.TApplication;
 import kong.unirest.UnirestInstance;
 import lombok.SneakyThrows;
+import org.fusesource.jansi.AnsiConsole;
+import org.jline.keymap.KeyMap;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.LineReaderImpl;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fusesource.jansi.Ansi.Color.*;
+import static org.fusesource.jansi.Ansi.ansi;
 
 @ReflectiveAccess
 @Command(name = "create-wizard")
@@ -43,27 +56,83 @@ public class SSCAppVersionCreateWizardCommand extends AbstractSSCUnirestRunnerCo
 	@CommandLine.Mixin private OutputMixin outputMixin;
 	@CommandLine.Mixin private SSCFilterMixin sscFilterMixin;
 
-	@CommandLine.Option(names = {"-t"}, description = "ECMA48, SWING, XTERM")
-	public TApplication.BackendType backendType;
+	private void jline3Test() throws IOException {
+		System.setProperty("org.jline.terminal.dumb", "true");
 
-	private class MyApplication extends TApplication {
+		// JANSI test
+		AnsiConsole.systemInstall();
 
-		public MyApplication() throws Exception {
-			super(backendType);
+		// JLine3 test
+		Terminal terminal = TerminalBuilder.terminal();
+		terminal.enterRawMode();
 
-			// Create standard menus for Tool, File, and Window.
-			addToolMenu();
-			addFileMenu();
-			addWindowMenu();
-		}
+		LineReader reader = LineReaderBuilder.builder()
+				.terminal(terminal)
+				.build();
+
+		ArrayList<String> options = new ArrayList<>();
+		options.add("option-1");
+		options.add("option-2");
+		options.add("option-3");
+		int selected = menu(terminal, reader, options);
+
+		terminal.writer().println(String.format("\nYou selected: %s", options.get(selected)));
+		terminal.writer().flush();
+		// JLine3 test : END
 	}
 
+	private void drawMenu(Terminal terminal, List<String> options, int selectedOption){
+		int pos = 0;
 
+		terminal.writer().print(ansi().eraseScreen().reset());
+		terminal.writer().flush();
+		//System.out.println(ansi().eraseScreen().reset());
+		for (String opt : options){
+			if(pos == selectedOption){
+				terminal.writer().println(ansi().bg(255).fg(0).a(opt).reset());
+				//System.out.println(ansi().bg(255).fg(0).a(opt).reset());
+			}else {
+				terminal.writer().println(ansi().bg(0).fg(255).a(opt).reset());
+				//System.out.println(ansi().bg(0).fg(255).a(opt).reset());
+			}
+			pos++;
+		}
+		terminal.writer().flush();
+	}
 
-		@SneakyThrows
+	private int menu(Terminal terminal, LineReader reader, List<String> options){
+		int selection = 0;
+		// draw initial menu
+		drawMenu(terminal, options, selection);
+
+		// loop:
+		while (true) {
+			int c = ((LineReaderImpl) reader).readCharacter();
+			if(c == 119){ // up
+				if(selection == 0){
+					continue;
+				}
+				selection--;
+				drawMenu(terminal, options, selection);
+				continue;
+			}else if(c == 115){ // down
+				if(selection == options.size()-1){
+					continue;
+				}
+				selection++;
+				drawMenu(terminal, options, selection);
+				continue;
+			}
+			if (c == 10 || c == 13) return selection;
+		}
+			// read key
+			// if needed, redraw updated menu
+			// if needed return selection
+	}
+
+	@SneakyThrows
 	protected Void runWithUnirest(UnirestInstance unirest) {
-		MyApplication app = new MyApplication();
-		app.run();
+		jline3Test();
 		return null;
 	}
 	
