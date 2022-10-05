69877d0b7ec3621a85bd44af03d0c27dc1b13dd3
Ruud Senden <ruud.senden@microfocus.com>
2022-10-05 21:39:29 +0200

    Add scan actions functionality

--- fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/scan/helper/SCDastScanHelper.java
+++ fcli-sc-dast/src/main/java/com/fortify/cli/sc_dast/scan/helper/SCDastScanHelper.java
@@ -25,6 +25,8 @@
 package com.fortify.cli.sc_dast.scan.helper;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fortify.cli.common.json.JsonHelper;
 
 import kong.unirest.UnirestInstance;
@@ -32,9 +34,24 @@ import kong.unirest.UnirestInstance;
 public class SCDastScanHelper {
     private SCDastScanHelper() {}
     
+    public static enum ScanAction {
+        PauseScan, ResumeScan, DeleteScan, ClearTrackedScan, RetryImportScanResults, CompleteScan, RetryImportScanFindings
+    }
+    
     public static final SCDastScanDescriptor getScanDescriptor(UnirestInstance unirest, String scanId) {
         return getDescriptor(
                 unirest.get(String.format("/api/v2/scans/%s/scan-summary", scanId)).asObject(JsonNode.class).getBody());
+    }
+    
+    public static final SCDastScanDescriptor performScanAction(UnirestInstance unirest, SCDastScanDescriptor descriptor, ScanAction action) {
+        ObjectNode data = new ObjectMapper().createObjectNode().put("scanActionType", action.name());
+        unirest.post("/api/v2/scans/{scanId}/scan-action")
+            .routeParam("scanId", descriptor.getId())
+            .body(data)
+            .asObject(JsonNode.class).getBody();
+        descriptor = getScanDescriptor(unirest, descriptor.getId());
+        descriptor.asObjectNode().put("action", action.name().toUpperCase()+"_REQUESTED");
+        return descriptor;
     }
 
     private static final SCDastScanDescriptor getDescriptor(JsonNode sensorNode) {
