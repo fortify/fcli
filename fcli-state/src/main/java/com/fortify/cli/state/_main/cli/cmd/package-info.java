2d3d5e63e3f10544c3e7d3dddee103f88870c44f
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-03-20 09:27:29 +0100

    chore: Minor JavaDoc & resource bundle updates

--- fcli-state/src/main/java/com/fortify/cli/state/_main/cli/cmd/package-info.java
+++ fcli-state/src/main/java/com/fortify/cli/state/_main/cli/cmd/package-info.java
@@ -1,8 +1,6 @@
 /**
- * This package defines the 'config' top-level command, some generic sub-commands, and any accompanying 
- * classes like mixins and (abstract) base classes (at the time of writing there are no accompanying classes).
- * At the moment there's only a small number of configuration commands, we could consider using some command
- * hierarchy if we think the list of direct sub-commands grows too large.
+ * This package defines the 'state' top-level command, some generic sub-commands, and any accompanying 
+ * classes like mixins and (abstract) base classes.
  */
 package com.fortify.cli.state._main.cli.cmd;
 
