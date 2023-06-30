d751072e757174366f98baf5389a50e04cf67361
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-06-30 16:05:33 +0200

    chore: Replace non-ascii characters in copyright statements

--- fcli-tool/src/main/resources/com/fortify/cli/tool/fod-uploader/extra-files/bin/FoDUpload.bat
+++ fcli-tool/src/main/resources/com/fortify/cli/tool/fod-uploader/extra-files/bin/FoDUpload.bat
@@ -2,7 +2,7 @@
 @rem Copyright 2021, 2022 Open Text.
 @rem
 @rem The only warranties for products and services of Open Text 
-@rem and its affiliates and licensors (“Open Text”) are as may 
+@rem and its affiliates and licensors ("Open Text") are as may 
 @rem be set forth in the express warranty statements accompanying 
 @rem such products and services. Nothing herein should be construed 
 @rem as constituting an additional warranty. Open Text shall not be 
--- integration-test/ssc-integration-test.sh
+++ integration-test/ssc-integration-test.sh
@@ -3,7 +3,7 @@
 # Copyright 2021, 2023 Open Text.
 #
 # The only warranties for products and services of Open Text 
-# and its affiliates and licensors (“Open Text”) are as may 
+# and its affiliates and licensors ("Open Text") are as may 
 # be set forth in the express warranty statements accompanying 
 # such products and services. Nothing herein should be construed 
 # as constituting an additional warranty. Open Text shall not be 
