c0463d5a3a76601dede84254f60e11fc44e5ab36
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-07-17 17:36:32 +0200

    chore: Show original exception on expression exception

--- fcli-core/fcli-common/src/main/java/com/fortify/cli/common/output/query/QueryExpression.java
+++ fcli-core/fcli-common/src/main/java/com/fortify/cli/common/output/query/QueryExpression.java
@@ -29,7 +29,7 @@ public class QueryExpression {
         try {
             return JsonHelper.evaluateSpelExpression(record, expression, Boolean.class);
         } catch ( Exception e ) {
-            throw new IllegalStateException(String.format("Error evaluating query expression:\n\tMessage: %s\n\tExpression: %s\n\tRecord: %s", e.getMessage(), expression.getExpressionString(), record.toPrettyString().replace("\n", "\n\t\t")));
+            throw new IllegalStateException(String.format("Error evaluating query expression:\n\tMessage: %s\n\tExpression: %s\n\tRecord: %s", e.getMessage(), expression.getExpressionString(), record.toPrettyString().replace("\n", "\n\t\t")), e);
         }
     }
 }
