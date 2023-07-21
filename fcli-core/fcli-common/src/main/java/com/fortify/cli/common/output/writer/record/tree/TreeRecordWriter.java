0a3de1bc14591aca2cbabe8b181428ad614a4fef
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-07-21 16:17:16 +0200

    fix: Output all contents in tree output (fixes #104)
    fixed array handling, switched output from ascii to unicode

--- fcli-core/fcli-common/src/main/java/com/fortify/cli/common/output/writer/record/tree/TreeRecordWriter.java
+++ fcli-core/fcli-common/src/main/java/com/fortify/cli/common/output/writer/record/tree/TreeRecordWriter.java
@@ -22,7 +22,6 @@ import com.fortify.cli.common.output.writer.record.AbstractFormattedRecordWriter
 import com.fortify.cli.common.output.writer.record.RecordWriterConfig;
 
 import hu.webarticum.treeprinter.SimpleTreeNode;
-//import hu.webarticum.treeprinter.ListingTreePrinter;
 import hu.webarticum.treeprinter.printer.listing.ListingTreePrinter;
 
 // TODO Use PrintWriter from RecordWriterConfig, wo allow output to file
@@ -33,40 +32,53 @@ public class TreeRecordWriter extends AbstractFormattedRecordWriter {
 
     @Override
     public void writeFormattedRecord(ObjectNode record) {
-        SimpleTreeNode rootNode = new SimpleTreeNode("-+-");
-        treeBuilder(rootNode, record, null);
-        ListingTreePrinter.builder().ascii().build().print(rootNode); // TODO print to actual output, but for some reason line below doesn't work
-        //ListingTreePrinter.createBuilder().ascii().build().print(rootNode, config.getPrintWriterSupplier().get());  // print with ascii
-        //new ListingTreePrinter().print(rootNode); // print with unicode
+        SimpleTreeNode rootNode = new SimpleTreeNode("─┬─");
+        treeBuilder(rootNode, record);
+        //We previously printed in ascii, waiting on feedback from MS if there was a specific reason for that
+        //ListingTreePrinter.builder().ascii().build().print(rootNode); 
+        new ListingTreePrinter().print(rootNode); // print with unicode
     }
 
-    private static void treeBuilder(SimpleTreeNode treeNode, JsonNode inputNode, String firstLevelLabelSuffix){
-        firstLevelLabelSuffix = (firstLevelLabelSuffix == null) ? "" : firstLevelLabelSuffix;
-        if( inputNode.getNodeType() == JsonNodeType.ARRAY){
-            int cnt = 0;
+    private static void treeBuilder(SimpleTreeNode treeNode, JsonNode inputNode){
+        JsonNodeType nodeType = inputNode.getNodeType();
+        
+        switch(nodeType) {
+        case NUMBER:
+        case STRING:
+        case BOOLEAN:
+        	addPrimitiveChildNode(treeNode, inputNode.asText());
+        	break;
+        case OBJECT:
+            addObjectChildNode(treeNode, inputNode);
+            break;
+        case ARRAY:
             for (JsonNode n: inputNode) {
-                SimpleTreeNode childNode = new SimpleTreeNode(String.format("#%d:%s", cnt, firstLevelLabelSuffix) );
-                treeBuilder(childNode, n, null);
-                treeNode.addChild(childNode);
-                cnt++;
+                treeBuilder(treeNode, n);
             }
-        }else if(inputNode.getNodeType() == JsonNodeType.OBJECT){
-            for (Iterator<Map.Entry<String, JsonNode>> it = inputNode.fields(); it.hasNext(); ) {
-                Map.Entry<String, JsonNode> n = it.next();
-                if(n.getValue().getNodeType() != JsonNodeType.OBJECT){
-                    SimpleTreeNode childNode = new SimpleTreeNode( n.getKey() + ": " + n.getValue().asText());
-                    treeNode.addChild(childNode);
-                }else if(n.getValue().getNodeType() == JsonNodeType.OBJECT){
-                    SimpleTreeNode childNode = new SimpleTreeNode(n.getKey());
-                    treeBuilder(childNode, n.getValue(), null);
-                    treeNode.addChild(childNode);
-                }
-            }
-        } else if(inputNode.getNodeType() == JsonNodeType.NUMBER || inputNode.getNodeType() == JsonNodeType.STRING ) {
-            SimpleTreeNode childNode = new SimpleTreeNode( inputNode.asText());
-            treeNode.addChild(childNode);
+            break;
+		default:
+			break;
         }
 
+    }
+    
+    private static void addPrimitiveChildNode(SimpleTreeNode treeNode, String text) {
+    	SimpleTreeNode childNode = new SimpleTreeNode( text );
+        treeNode.addChild(childNode);
+    }
+    
+    private static void addObjectChildNode(SimpleTreeNode treeNode, JsonNode inputNode) {
+
+        for (Iterator<Map.Entry<String, JsonNode>> it = inputNode.fields(); it.hasNext(); ) {
+            Map.Entry<String, JsonNode> n = it.next();
+            if(n.getValue().isContainerNode()) {
+            	SimpleTreeNode childNode = new SimpleTreeNode(n.getKey());
+                treeBuilder(childNode, n.getValue());
+                treeNode.addChild(childNode);
+            } else {
+            	addPrimitiveChildNode(treeNode, n.getKey() + ": " + n.getValue().asText());
+            }
+        }
     }
 
 }
