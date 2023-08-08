7acc319fa10a7373a59bda7f64439bcbbbea24f5
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-08-08 22:31:24 +0200

    feat: added ssc user create command
    added ssc user create command to create local ssc users

---
+++ fcli-core/fcli-ssc/src/main/java/com/fortify/cli/ssc/user/helper/SSCRoleObject.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright 2021, 2023 Open Text.
+ *
+ * The only warranties for products and services of Open Text
+ * and its affiliates and licensors ("Open Text") are as may
+ * be set forth in the express warranty statements accompanying
+ * such products and services. Nothing herein should be construed
+ * as constituting an additional warranty. Open Text shall not be
+ * liable for technical or editorial errors or omissions contained
+ * herein. The information contained herein is subject to change
+ * without notice.
+ *******************************************************************************/
+package com.fortify.cli.ssc.user.helper;
+
+import lombok.Getter;
+
+public class SSCRoleObject {
+    @Getter private String id;
+    public SSCRoleObject(String id) {
+        this.id = id;
+    }
+}
+
