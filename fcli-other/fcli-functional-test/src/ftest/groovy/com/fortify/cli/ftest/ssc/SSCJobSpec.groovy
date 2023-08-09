f03f75c71d7f771a1137f700260623a75b36621d
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-08-09 23:53:26 +0200

    chore: updated tests, added spock annotation extension

--- fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/ssc/SSCJobSpec.groovy
+++ fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/ssc/SSCJobSpec.groovy
@@ -26,27 +26,43 @@ import spock.lang.Requires
 import spock.lang.Shared
 import spock.lang.Stepwise
 import spock.lang.Unroll
+import com.fortify.cli.ftest._common.StepWiseExcept
 
-@Prefix("ssc.job") @FcliSession(SSC) @Stepwise
+@Prefix("ssc.job") @FcliSession(SSC) @StepWiseExcept(except="ssc.job (SSCJobSpec).upload,second value")
 class SSCJobSpec extends FcliBaseSpec {
     @Shared @AutoCleanup SSCAppVersion version = new SSCAppVersion().create()
     @Shared @TestResource("runtime/shared/EightBall-22.1.0.fpr") String fpr
+    @Shared @TestResource("runtime/shared/LoginProject.fpr") String diffpr
     @Shared String uploadVariableName = version.fcliVariableName+"_artifact"
     @Shared String uploadVariableRef = "::$uploadVariableName::"
+    @Shared int repeats = 5;
 
-    //these uploads are here to create cancellable jobs
+    /*
+     * these uploads are here to create cancellable jobs
+     * the most reliable way seems to be to upload differing artifacts
+     * and then approve the last one uploaded so multiple processing jobs are scheduled
+     * this assumes that the processing rule with identifier
+     * "com.fortify.manager.BLL.processingrules.VetoCascadingApprovalProcessingRule"
+     * is NOT enabled on the application version in question
+     */
+    @Unroll
     def "upload"() {
         def args = "ssc artifact upload $fpr --appversion "+ 
             "${version.variableRef} --store $uploadVariableName"
         when:
             def result = Fcli.run(args)
+            fpr = diffpr
+            if(i==repeats) {
+            Fcli.run("ssc artifact wait-for $uploadVariableRef -i 1s -s PROCESS_COMPLETE,REQUIRE_AUTH")
+            Fcli.run("ssc artifact approve ::$uploadVariableName::id")
+            }
         then:
             verifyAll(result.stdout) {
                 it[0] =~ /Id\s+Scan types\s+Last scan date\s+Upload date\s+Status/
                 it[1] =~ /^\s*\d+.*/
             }
         where:
-            i << (1..5)
+            i << (1..repeats)
     }
     
     def "list"() {
@@ -62,7 +78,7 @@ class SSCJobSpec extends FcliBaseSpec {
     }
     
     def "update"() {
-        def args = "ssc job update ::jobs::get(#var('jobs').size()-1).jobName --priority 1"
+        def args = "ssc job update ::jobs::get(#var('jobs').size()-1).jobName --priority 1 --store job"
         when:
             def result = Fcli.run(args)
         then:
@@ -72,7 +88,7 @@ class SSCJobSpec extends FcliBaseSpec {
     }
 
     def "cancel"() {
-        def args = "ssc job cancel ::jobs::get(0).jobName"
+        def args = "ssc job cancel ::job::jobName"
         when:
             def result = Fcli.run(args)
         then:
@@ -83,7 +99,7 @@ class SSCJobSpec extends FcliBaseSpec {
     }
 
     def "get.byName"() {
-        def args = "ssc job get ::jobs::get(0).jobName"
+        def args = "ssc job get ::job::jobName"
         when:
             def result = Fcli.run(args)
         then:
