ae94e057dd8fdbe369c8eac4c6dc21b87fea3fad
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-09-19 17:58:54 +0200

    chore: update FoDReleaseImportSpec
    add wait function to avoid errors on autoCleanup

--- fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/fod/FoDReleaseImportSpec.groovy
+++ fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/fod/FoDReleaseImportSpec.groovy
@@ -9,9 +9,11 @@ import com.fortify.cli.ftest._common.spec.Prefix
 import com.fortify.cli.ftest._common.spec.TestResource
 import com.fortify.cli.ftest.fod._common.FoDMobileAppSupplier
 import com.fortify.cli.ftest.fod._common.FoDWebAppSupplier
-
+import groovy.time.TimeDuration
 import spock.lang.AutoCleanup
 import spock.lang.Shared
+import org.hamcrest.Matchers
+import org.hamcrest.collection.ArrayMatching
 
 @Prefix("fod.import-scan") @FcliSession(FOD)
 class FoDReleaseImportSpec extends FcliBaseSpec {
@@ -22,6 +24,17 @@ class FoDReleaseImportSpec extends FcliBaseSpec {
     @Shared @AutoCleanup FoDWebAppSupplier webApp = new FoDWebAppSupplier()
     @Shared @AutoCleanup FoDMobileAppSupplier mobileApp = new FoDMobileAppSupplier()
 
+    
+    def "import-mobile"() {
+        def args = "fod release import-mobile --release ${mobileApp.get().qualifiedRelease} $mobileResults --store upload"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it.last().contains("IMPORT_REQUESTED")
+            }
+    }
     
     def "import-sast"() {
         def args = "fod release import-sast --release ${webApp.get().qualifiedRelease} $sastResults --store upload"
@@ -56,15 +69,44 @@ class FoDReleaseImportSpec extends FcliBaseSpec {
             }
     }
     
-    def "import-mobile"() {
-        def args = "fod release import-mobile --release ${mobileApp.get().qualifiedRelease} $mobileResults --store upload"
+    def "waitForScans"() {
         when:
-            def result = Fcli.run(args)
-        then:
-            verifyAll(result.stdout) {
-                size()>2
-                it.last().contains("IMPORT_REQUESTED")
+            def relScanurl = Fcli.run("fod release get ${webApp.get().qualifiedRelease} -o expr=/api/v3/releases/{releaseId}/scans --store relId").stdout[0]
+            def timeoutMs = 60000
+            def start = System.currentTimeMillis()
+            def success = false;
+            while(true){
+                def result = Fcli.run("fod rest call ${relScanurl}")
+                if(result.stdout.findAll{element -> element.contains("analysisStatusType: \"Completed\"")}.size()==3) {
+                    success=true;
+                    break;
+                } else if(System.currentTimeMillis()-start > timeoutMs) {
+                    break;
+                }
+                sleep(3000)
             }
+        then:
+            success
+    }
+    
+    def "waitForScans2"() {
+        when:
+            def relScanurl = Fcli.run("fod release get ${mobileApp.get().qualifiedRelease} -o expr=/api/v3/releases/{releaseId}/scans --store relId").stdout[0]
+            def timeoutMs = 60000
+            def start = System.currentTimeMillis()
+            def success = false;
+            while(true){
+                def result = Fcli.run("fod rest call ${relScanurl}")
+                if(result.stdout.findAll{element -> element.contains("analysisStatusType: \"Completed\"")}.size()==1) {
+                    success=true;
+                    break;
+                } else if(System.currentTimeMillis()-start > timeoutMs) {
+                    break;
+                }
+                sleep(3000)
+            }
+        then:
+            success
     }
     
 }
