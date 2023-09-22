d38f53b936aa27748b1d32de820fa436bbed669e
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-09-22 15:57:45 +0200

    chore: added bugtracker-utillity tool commands
    added bugtracker-utility commands, adjusted AbstractToolUninstallCommand to accept "latest" or "default" version and translate to proper version number

---
+++ fcli-core/fcli-tool/src/main/java/com/fortify/cli/tool/bugtracker_utility/cli/cmd/ToolBugTrackerUtilityInstallCommand.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright 2021, 2023 Open Text.
+ *
+ * The only warranties for products and services of Open Text 
+ * and its affiliates and licensors ("Open Text") are as may 
+ * be set forth in the express warranty statements accompanying 
+ * such products and services. Nothing herein should be construed 
+ * as constituting an additional warranty. Open Text shall not be 
+ * liable for technical or editorial errors or omissions contained 
+ * herein. The information contained herein is subject to change 
+ * without notice.
+ *******************************************************************************/
+package com.fortify.cli.tool.bugtracker_utility.cli.cmd;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.fortify.cli.common.output.cli.mixin.OutputHelperMixins;
+import com.fortify.cli.tool._common.cli.cmd.AbstractToolInstallCommand;
+import com.fortify.cli.tool._common.helper.ToolHelper;
+import com.fortify.cli.tool._common.helper.ToolVersionInstallDescriptor;
+import com.fortify.cli.tool._common.util.FileUtils;
+
+import lombok.Getter;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+
+@Command(name = OutputHelperMixins.Install.CMD_NAME)
+public class ToolBugTrackerUtilityInstallCommand extends AbstractToolInstallCommand {
+    @Getter @Mixin private OutputHelperMixins.Install outputHelper;
+    @Getter private String toolName = ToolBugTrackerUtilityCommands.TOOL_NAME;
+    
+    @Override
+    protected InstallType getInstallType() {
+        return InstallType.EXTRACT_ZIP;
+    }
+    
+    @Override
+    protected void postInstall(ToolVersionInstallDescriptor descriptor) throws IOException {
+        Path binPath = descriptor.getBinPath();
+        Files.createDirectories(binPath);
+        FileUtils.copyResourceToDir(ToolHelper.getResourceFile(getToolName(), "extra-files/bin/FortifyBugTrackerUtility"), binPath);
+        FileUtils.copyResourceToDir(ToolHelper.getResourceFile(getToolName(), "extra-files/bin/FortifyBugTrackerUtility.bat"), binPath);
+        
+        String version = descriptor.getOriginalDownloadDescriptor().getVersion();
+        String jarName = String.format("FortifyBugTrackerUtility-%s.jar", version);
+        
+        descriptor.getInstallPath().resolve(jarName).toFile().renameTo(
+                descriptor.getInstallPath().resolve("FortifyBugTrackerUtility.jar").toFile());
+        
+    }
+}
