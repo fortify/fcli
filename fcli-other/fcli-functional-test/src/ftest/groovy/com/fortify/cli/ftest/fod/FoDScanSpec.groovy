04127bbbb60c48319e294d19482682d842100455
psmf22 <117990501+psmf22@users.noreply.github.com>
2023-10-24 12:13:25 +0200

    chore: update scan import description

--- fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/fod/FoDScanSpec.groovy
+++ fcli-other/fcli-functional-test/src/ftest/groovy/com/fortify/cli/ftest/fod/FoDScanSpec.groovy
@@ -9,6 +9,7 @@ import com.fortify.cli.ftest._common.spec.Prefix
 import com.fortify.cli.ftest._common.spec.TestResource
 import com.fortify.cli.ftest.fod._common.FoDWebAppSupplier
 import com.fortify.cli.ftest.fod._common.FoDUserSupplier
+import com.fortify.cli.ftest.fod._common.FoDMobileAppSupplier
 import com.fortify.cli.ftest.fod._common.FoDUserGroupSupplier
 
 import spock.lang.AutoCleanup
@@ -19,13 +20,60 @@ import spock.lang.Unroll
 @Prefix("fod.scan") @FcliSession(FOD) @Stepwise
 class FoDScanSpec extends FcliBaseSpec {
     @Shared @TestResource("runtime/shared/EightBall-22.1.0.fpr") String sastResults
-    //@Shared @TestResource("runtime/shared/iwa_net_scandata.fpr") String dastResults
-    //@Shared @TestResource("runtime/shared/iwa_net_cyclonedx.json") String ossResults
-    @Shared @AutoCleanup FoDWebAppSupplier app = new FoDWebAppSupplier()
+    @Shared @TestResource("runtime/shared/iwa_net_scandata.fpr") String dastResults
+    @Shared @TestResource("runtime/shared/iwa_net_cyclonedx.json") String ossResults
+    @Shared @TestResource("runtime/shared/iwa_mobile.fpr") String mobileResults
+    @Shared FoDWebAppSupplier webApp = new FoDWebAppSupplier()
+    @Shared @AutoCleanup FoDMobileAppSupplier mobileApp = new FoDMobileAppSupplier()
 
+    
+    
+    def "import-sast"() {
+        def args = "fod sast-scan import --release=${webApp.get().qualifiedRelease} --file=$sastResults --store uploadsast"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it.last().contains("IMPORT_REQUESTED")
+            }
+    }
     /*
-    def "get.byId"() {
-        def args = "fod sast-scan get ::scans::get(0).scanId"
+    def "import-mobile"() {
+        def args = "fod mast-scan import --release=${mobileApp.get().qualifiedRelease} --file=$mobileResults --store uploadmast"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it.last().contains("IMPORT_REQUESTED")
+            }
+    }
+
+    def "import-dast"() {
+        def args = "fod dast-scan import --release=${webApp.get().qualifiedRelease} --file=$dastResults --store uploaddast"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it.last().contains("IMPORT_REQUESTED")
+            }
+    }
+
+    def "import-oss"() {
+        def args = "fod oss-scan import --release=${webApp.get().qualifiedRelease} --file=$ossResults --store uploadoss"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it.last().contains("IMPORT_REQUESTED")
+            }
+    }*/
+
+    def "waitForSast"() {
+        def args = "fod sast-scan wait-for ::uploadsast::"
         when:
             def result = Fcli.run(args)
         then:
@@ -34,7 +82,20 @@ class FoDScanSpec extends FcliBaseSpec {
                 it[1].startsWith("startedByUserId: ")
             }
     }
+
     
+    def "get.byId"() {
+        //def args = "fod sast-scan get ::scans::get(0).scanId"
+        def args = "util all-commands list"
+        when:
+            def result = Fcli.run(args)
+        then:
+            verifyAll(result.stdout) {
+                size()>2
+                it[1].startsWith("startedByUserId: ")
+            }
+    }
+    /*
     def "import-sast"() {
         //get release id
         def appRelId = Fcli.run("fod release get " + app.appName + ":" + app.versionName + " --store release")
