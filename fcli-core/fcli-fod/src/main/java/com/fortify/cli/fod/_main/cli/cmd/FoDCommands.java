2b97d5c95e56159e216de154b9671a057367a0c7
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-04-02 17:45:31 +0200

    feat: Add `fcli fod issue list` command

--- fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_main/cli/cmd/FoDCommands.java
+++ fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_main/cli/cmd/FoDCommands.java
@@ -18,6 +18,7 @@ import com.fortify.cli.fod.access_control.cli.cmd.FoDAccessControlCommands;
 import com.fortify.cli.fod.action.cli.cmd.FoDActionCommands;
 import com.fortify.cli.fod.app.cli.cmd.FoDAppCommands;
 import com.fortify.cli.fod.dast_scan.cli.cmd.FoDDastScanCommands;
+import com.fortify.cli.fod.issue.cli.cmd.FoDIssueCommands;
 import com.fortify.cli.fod.mast_scan.cli.cmd.FoDMastScanCommands;
 import com.fortify.cli.fod.microservice.cli.cmd.FoDMicroserviceCommands;
 import com.fortify.cli.fod.oss_scan.cli.cmd.FoDOssScanCommands;
@@ -52,6 +53,7 @@ import picocli.CommandLine.Command;
                 FoDDastScanCommands.class,
                 FoDMastScanCommands.class,
                 FoDOssScanCommands.class,
+                FoDIssueCommands.class,
                 FoDReportCommands.class,
                 FoDRestCommands.class,
 
