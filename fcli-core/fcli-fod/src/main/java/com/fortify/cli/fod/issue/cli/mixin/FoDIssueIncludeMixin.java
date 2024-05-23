9a6e713ab356fd6d3f73441d02cb25df9609c864
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-05-23 12:53:33 +0200

    chore: `fcli fod issue ls --include`: Fixes & improvements

--- fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/issue/cli/mixin/FoDIssueIncludeMixin.java
+++ fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/issue/cli/mixin/FoDIssueIncludeMixin.java
@@ -1,19 +1,26 @@
 package com.fortify.cli.fod.issue.cli.mixin;
 
+import java.util.List;
+
 import com.fortify.cli.common.rest.unirest.IHttpRequestUpdater;
+import com.fortify.cli.common.util.DisableTest;
+import com.fortify.cli.common.util.DisableTest.TestType;
+
 import kong.unirest.HttpRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import picocli.CommandLine;
-import java.util.List;
+import picocli.CommandLine.Option;
 
 public class FoDIssueIncludeMixin implements IHttpRequestUpdater {
-    @CommandLine.Option(names = {"--include", "-i"}, split = ",", descriptionKey = "fcli.fod.issue.list.includeIssue")
+    @DisableTest(TestType.MULTI_OPT_PLURAL_NAME)
+    @Option(names = {"--include", "-i"}, split = ",", descriptionKey = "fcli.fod.issue.list.includeIssue", paramLabel="<status>")
     private List<FoDIssueInclude> includes;
 
     public HttpRequest<?> updateRequest(HttpRequest<?> request) {
-        for ( var include : includes) {
-            request = request.queryString(include.getRequestParameterName(), "true");
+        if ( includes!=null ) {
+            for ( var include : includes) {
+                request = request.queryString(include.getRequestParameterName(), "true");
+            }
         }
         return request;
     }
