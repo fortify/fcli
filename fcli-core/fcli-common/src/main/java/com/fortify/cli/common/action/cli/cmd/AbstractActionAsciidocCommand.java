3321113393e3a4d0e943b9a740c50624b7f83907
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-09-17 12:39:22 +0200

    docs: Improve action synopsis

--- fcli-core/fcli-common/src/main/java/com/fortify/cli/common/action/cli/cmd/AbstractActionAsciidocCommand.java
+++ fcli-core/fcli-common/src/main/java/com/fortify/cli/common/action/cli/cmd/AbstractActionAsciidocCommand.java
@@ -90,7 +90,7 @@ public abstract class AbstractActionAsciidocCommand extends AbstractRunnableComm
             
             === Synopsis
             
-            *fcli ${typeLower} action run %s <options>* 
+            *fcli ${typeLower} action run %s link:manpage/fcli-${typeLower}-action-run.html#_options[[fcli ${typeLower} action run options]] [action options, see below]* 
             
             === Description
             
