1c5f9d99b9f5d4f4f94b9822543a810d2626189a
cdatla <cdatla@opentext.com>
2025-10-09 15:37:14 +0530

    chore: move preflight check before fpr download

--- fcli-core/fcli-aviator/src/main/java/com/fortify/cli/aviator/ssc/cli/cmd/AviatorSSCAuditCommand.java
+++ fcli-core/fcli-aviator/src/main/java/com/fortify/cli/aviator/ssc/cli/cmd/AviatorSSCAuditCommand.java
@@ -64,6 +64,12 @@ public class AviatorSSCAuditCommand extends AbstractSSCJsonNodeOutputCommand imp
             AviatorLoggerImpl logger = new AviatorLoggerImpl(progressWriter);
             SSCAppVersionDescriptor av = appVersionResolver.getAppVersionDescriptor(unirest);
 
+            long auditableIssueCount = AviatorSSCAuditHelper.getAuditableIssueCount(unirest, av, logger, noFilterSet, filterSetOptions, folderNames);
+            if (auditableIssueCount == 0) {
+                logger.progress("Audit skipped - no auditable issues found matching the specified filters.");
+                return AviatorSSCAuditHelper.buildResultNode(av, "N/A", "SKIPPED (no auditable issues)");
+            }
+
             downloadedFprPath = downloadFpr(unirest, av, logger);
             if (downloadedFprPath == null) {
                 return AviatorSSCAuditHelper.buildResultNode(av, "N/A", "SKIPPED (no FPR available to audit)");
@@ -79,12 +85,6 @@ public class AviatorSSCAuditCommand extends AbstractSSCJsonNodeOutputCommand imp
 
     @SneakyThrows
     private JsonNode processFpr(UnirestInstance unirest, SSCAppVersionDescriptor av, String token, String url, AviatorLoggerImpl logger, Path downloadedFprPath) {
-        long auditableIssueCount = AviatorSSCAuditHelper.getAuditableIssueCount(unirest, av, logger, noFilterSet, filterSetOptions, folderNames);
-        if (auditableIssueCount == 0) {
-            logger.progress("Audit skipped - no auditable issues found matching the specified filters.");
-            return AviatorSSCAuditHelper.buildResultNode(av, "N/A", "SKIPPED (no auditable issues)");
-        }
-
         FPRAuditResult auditResult;
 
         try (FprHandle fprHandle = new FprHandle(downloadedFprPath)) {
