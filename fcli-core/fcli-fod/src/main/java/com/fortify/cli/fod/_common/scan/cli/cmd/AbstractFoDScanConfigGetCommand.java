596a6961e05cfcbe808c689db5fb038af3c3097f
kadraman <klee2@opentext.com>
2026-04-20 16:36:30 +0100

    chore: Updated `fod dast-scan setup-xxx` commands  to display message if settings are locked.
    chore: Updated `fod dast-scan get-config` to return empty descriptor when not setup rather than throw exception.

--- fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_common/scan/cli/cmd/AbstractFoDScanConfigGetCommand.java
+++ fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_common/scan/cli/cmd/AbstractFoDScanConfigGetCommand.java
@@ -13,8 +13,8 @@
 package com.fortify.cli.fod._common.scan.cli.cmd;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fortify.cli.common.cli.util.CommandGroup;
-import com.fortify.cli.common.exception.FcliSimpleException;
 import com.fortify.cli.common.json.JsonNodeHolder;
 import com.fortify.cli.common.util.DisableTest;
 import com.fortify.cli.common.util.DisableTest.TestType;
@@ -34,11 +34,10 @@ public abstract class AbstractFoDScanConfigGetCommand extends AbstractFoDJsonNod
     public final JsonNode getJsonNode(UnirestInstance unirest) {
         var releaseId = releaseResolver.getReleaseId(unirest);
         var descriptor = getDescriptor(unirest, releaseId);
-        if ( descriptor==null ) {
-            throw new FcliSimpleException("Release does not have any saved scan settings");
-        }
-        var result = descriptor.asObjectNode();
-        return result.get("assessmentTypeId").asText().equals("0")
+        var result = descriptor == null
+                ? JsonNodeFactory.instance.objectNode().put("assessmentTypeId", 0)
+                : descriptor.asObjectNode();
+        return result.path("assessmentTypeId").asInt(0) == 0
                 ? result.put("state", "Not configured")
                 : result.put("state", "Configured");
     }
--- fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_common/scan/helper/dast/FoDScanDastAutomatedHelper.java
+++ fcli-core/fcli-fod/src/main/java/com/fortify/cli/fod/_common/scan/helper/dast/FoDScanDastAutomatedHelper.java
@@ -51,7 +51,7 @@ public class FoDScanDastAutomatedHelper extends FoDScanHelper {
             if (e.getStatus() == 400) {
                 // use the internal FoD "errorCode" to determine if any settings have been defined
                 if (e.getMessage().contains("errorCode: 6000")) {
-                    throw new FcliSimpleException("Release does not have any saved scan settings");
+                    return null;
                 }
                 throw new FcliSimpleException("Bad request. Error: " + e.getMessage());
             }
@@ -86,7 +86,7 @@ public class FoDScanDastAutomatedHelper extends FoDScanHelper {
             for (JsonNode node : itemsNode) {
                 if (!"Dynamic".equals(node.path("scanType").asText())) continue;
                 String status = node.path("analysisStatusType").asText();
-                
+
                 if (isActiveStatus(status)) {
                     foundActive = true;
                     FoDScanDescriptor result = handleActiveScan(
